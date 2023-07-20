463c79cfe61f6c70b67ede13bbfa2c198c574221
Luka-Timtam <132308048+Luka-Timtam@users.noreply.github.com>
2023-07-21 10:58:35 +1200

    21/07

--- Components/watch ordering.py
+++ Components/watch ordering.py
@@ -4,11 +4,18 @@ watch_models = ['Casio Worldtime','Timex Weekender','Seiko Speedtimer','Tissot P
 #list of watch prices
 watch_prices = [70, 85, 800, 850, 2450, 6750, 7000, 8000, 16000, 55000, 650000, 7400000]
 
+#list to store ordered watches
+order_list = []
+#list to store watch prices
+order_cost = []
+
 
 def menu():
     number_watches = 12
     for count in range (number_watches):
         print("{} {} ${:.2f}" .format(count+1, watch_models[count], watch_prices[count]))
+
+menu()
 
 
 #ask for how mnay they would like to order
@@ -17,7 +24,7 @@ num_watches = 0
 print ("")
 print ("Due to scarcity, there is a limit of 12 items available for you to order. If you select more than 5 items, shipping is free. Otherwise there is a $9.00 shipping fee")
 
-num_watches = input("How many watches would you like to order? ")
+num_watches = int(input("How many watches would you like to order? "))
 
 print(num_watches)
 
@@ -25,7 +32,14 @@ print(num_watches)
 #choose watch from menu
 print ("Please choose your watches by entering the number from the menu")
 for item in range(num_watches):
+    while num_watches > 0:
+        watch_ordered = int(input())
+        order_list.append(watch_models[watch_ordered])
+        order_list.append(watch_prices[watch_ordered])
+        num_watches = num_watches-1
 
+print(order_list)
+print(order_cost)
 
 
 
