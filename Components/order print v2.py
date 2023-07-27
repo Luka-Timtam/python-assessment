5284a2887b25b79788a3ee4387db322406e519d0
Luka-Timtam <132308048+Luka-Timtam@users.noreply.github.com>
2023-07-27 12:33:14 +1200

    27/07 pt 2

---
+++ Components/order print v2.py
@@ -0,0 +1,14 @@
+#list to store ordered watches
+order_list = ['Casio Worldtime','Timex Weekender','Seiko Speedtimer','Tissot PRX 40mm']
+#list to store watch prices
+order_cost = [70, 85, 800, 850]
+
+customer_details = {"name":}
+
+
+#lesson 22
+
+count = 0
+for item in order_list:
+    print("Ordered: {} Cost: ${:.2f}".format(item, order_cost[count]))
+    count = count+1
