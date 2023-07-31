4fce2e923f8365f843d83eff5241d7c867d35466
Luka-Timtam <132308048+Luka-Timtam@users.noreply.github.com>
2023-08-01 10:44:18 +1200

    order print v4.py finished 1/08/23

--- Components/order print v4.py
+++ Components/order print v4.py
@@ -5,18 +5,19 @@ order_cost = [70, 85, 800, 850]
 
 customer_details = {'name':'Luka', 'phone':'0277282009', 'house':'26', 'street':'gracechurch', 'suburb':'flatbush'}
 
-
-#print(f"Customer Name: {customer_details['name']} \nCustomer Phone: {customer_details['phone']} \nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
-
-total_cost = sum(order_cost)
-
+def print_order():
+    total_cost = sum(order_cost) 
+    print("Customer Details")
+    print(f"Customer Name: {customer_details['name']} \nCustomer Phone: {customer_details['phone']} \nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
+    print()
+    print("Order Details")
+    count = 0
+    for item in order_list:
+        print("Ordered: {} Cost: ${:.2f}".format(item, order_cost[count]))
+        count = count+1
+    print()
+    print("Order Cost Details")
+    print(f"Total: ${total_cost}")
+print_order()
 
 #lesson 24
-
-count = 0
-for item in order_list:
-    print("Ordered: {} Cost: ${:.2f}".format(item, order_cost[count]))
-    count = count+1
-
-
-print(f"Your total order cost is:{total_cost}")
